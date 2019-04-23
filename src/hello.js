420d7c936849ec3822540c1948d87152175f5981
HamidRaza <hamidraza@users.noreply.github.com>
2019-04-23 20:29:28 +0530

    update props via onclick

--- src/hello.js
+++ src/hello.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 
-const Hello = ({ name, children }) => {
+const Hello = ({ name, children, onChange }) => {
   return (
     <div>
       <h1>Hello {name}</h1>
-      <Button color="primary" variant="contained">
+      <Button
+        color="primary"
+        variant="contained"
+        onClick={() => onChange("Ramesh")}
+      >
         Link
       </Button>
     </div>
--- src/index.js
+++ src/index.js
@@ -4,12 +4,21 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 import Hello from "./hello";
 
-function App() {
-  return (
-    <div className="App">
-      <Hello name="hamid">is dhakkan</Hello>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    name: "hamid"
+  };
+  _handleChange = name => {
+    this.setState({ name });
+  };
+  render() {
+    const { name } = this.state;
+    return (
+      <div className="App">
+        <Hello name={name} onChange={this._handleChange} />
+      </div>
+    );
+  }
 }
 
 const rootElement = document.getElementById("root");
